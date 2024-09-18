5780df6e51eea44867dee19d78ee75bc626bc368
AnroiDevel <anroidevel@gmail.com>
2024-09-18 14:23:54 +0300

    reliz

--- Assets/Game/Scripts/Finish.cs
+++ Assets/Game/Scripts/Finish.cs
@@ -4,6 +4,22 @@ namespace MultiTool
 {
     public class Finish : MonoBehaviour
     {
+
+        private void OnEnable()
+        {
+            
+        }
+
+        private void OnDisable()
+        {
+            
+        }
+
+        private void Awake()
+        {
+            
+        }
+
         private void OnTriggerEnter2D(Collider2D collision)
         {
             if(collision.gameObject.TryGetComponent(out PlayerController player))
